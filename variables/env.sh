4b46d3f07afb04ad44d6afdfdbf64712216496d1
Euan Mitchell <euanmitchell12@gmail.com>
2023-07-28 09:22:18 +0000

    working with shell variables

---
+++ variables/env.sh
@@ -0,0 +1,3 @@
+export SATELLITE="GEDI"
+
+echo "Variable 'SATELLITE' IS available when this script is sourced (i.e., 'source run.sh' prints this message and allows access to the variable)."
---
+++ variables/run.sh
@@ -0,0 +1,3 @@
+export ANIMAL="Bear"
+
+echo "Variable 'ANIMAL' is not available when this script is run (i.e., './run.sh' only prints this message)."
